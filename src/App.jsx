4c88ca8fd535c97a2b2d39335a461e64b313bbf1
uday <udaybasumatari103@gmail.com>
2026-02-05 09:53:01 +0530

    update tailwind and vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css';
+import "./App.css";
 
-import { useState } from 'react';
+import { useState } from "react";
 
-import { Button } from './components/ui/button';
+import Hero from "./components/custom/Hero";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>uday</h1>
-      <Button>click me</Button>
+      {/*Hero */}
+      <Hero />
     </>
   );
 }
---
+++ src/components/custom/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="flex items-center mx-56 gap-9">
+      <h1 className="font-extrabold text-[60px] text-center ">
+        <span> Discover Your Next Adventure with AI : </span> personalized
+        Itineraries at Your Fingertips
+      </h1>
+    </div>
+  );
+}
+
+export default Hero;
